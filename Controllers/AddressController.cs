f8a9d5f93ea2fb638b8f73c289d35d3ac5243f2b
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-11-28 18:58:43 -0300

    add create view and add method

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -16,4 +16,29 @@ public class AddressController : Controller
     {
         return View(_context.Addresses);
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add([FromForm] int id, [FromForm] string zipCode, [FromForm] string street, [FromForm] string number, [FromForm] string district, [FromForm] string city, [FromForm] string state)
+    {
+        Address address = new Address(id, zipCode, street, number, district, city, state);
+
+        if(_context.Addresses.Find(address.Id) == null)
+        {
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+        }
+        else
+        {
+            TempData["MessageError"] = $"Endereço com ID {address.Id} já existe.";
+            return RedirectToAction("Create");
+        }
+
+        TempData["MessageSuccess"] = $"Endereço com ID {address.Id} cadastrado com sucesso.";
+        return RedirectToAction("Index");
+    }
 }
