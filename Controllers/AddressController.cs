52117c5441cd70dc5f2951d0f5e94ac2e248dee8
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-11-28 19:53:19 -0300

    add update view and save method

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -41,4 +41,42 @@ public class AddressController : Controller
         TempData["MessageSuccess"] = $"Endereço com ID {address.Id} cadastrado com sucesso.";
         return RedirectToAction("Index");
     }
+
+    public IActionResult Update(int id)
+    {
+        Address address = _context.Addresses.Find(id);
+
+        if(address == null)
+        {
+            TempData["MessageError"] = $"Endereço com ID {id} não existe.";
+            return RedirectToAction("Index");
+        }
+
+        return View(address);
+    }
+
+    [HttpPost]
+    public IActionResult Save([FromForm] int id, [FromForm] string zipCode, [FromForm] string street, [FromForm] string number, [FromForm] string district, [FromForm] string city, [FromForm] string state)
+    {
+        Address address = _context.Addresses.Find(id);
+
+        if(address == null)
+        {
+            TempData["MessageError"] = $"Endereço com ID {id} não existe.";
+            return RedirectToAction("Update");
+        }
+
+        address.ZipCode = zipCode;
+        address.Street = street;
+        address.Number = number;
+        address.District = district;
+        address.City = city;
+        address.State = state;
+
+        _context.Addresses.Update(address);
+        _context.SaveChanges();
+
+        TempData["MessageSuccess"] = $"Endereço com ID {id} atualizado com sucesso.";
+        return RedirectToAction("Index");
+    }
 }
