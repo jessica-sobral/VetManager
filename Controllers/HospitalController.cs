a3934290c1a9a3408e0539a554c54714e688073d
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-11-28 23:00:39 -0300

    add update view and save method

--- Controllers/HospitalController.cs
+++ Controllers/HospitalController.cs
@@ -69,6 +69,54 @@ public class HospitalController : Controller
         _context.SaveChanges();
 
         TempData["MessageSuccess"] = $"Hospital com ID {hospital.Id} cadastrado com sucesso.";
-        return RedirectToAction("Index");
+        return RedirectToAction("Show", new { id = id });
+    }
+
+    public IActionResult Update(int id)
+    {
+        Hospital hospital = _context.Hospitals.Find(id);
+
+        if(hospital == null)
+        {
+            TempData["MessageError"] = $"Hospital com ID {id} não existe.";
+            return RedirectToAction("Index");
+        }
+
+        return View(hospital);
+    }
+
+    [HttpPost]
+    public IActionResult Save([FromForm] int id, [FromForm] string name, [FromForm] string telephone1, [FromForm] string telephone2, [FromForm] int addressId)
+    {
+        Hospital hospital = _context.Hospitals.Find(id);
+
+        if(_context.Addresses.Find(hospital.AddressId) == null)
+        {
+            TempData["MessageError"] = $"Endereço com ID {hospital.Id} não existe.";
+            return RedirectToAction("Create");
+        }
+
+        if(hospital == null)
+        {
+            TempData["MessageError"] = $"Hospital com ID {id} não existe.";
+            return RedirectToAction("Update");
+        }
+        
+        if(_context.Addresses.Find(hospital.AddressId) == null)
+        {
+            TempData["MessageError"] = $"Endereço com ID {id} não existe.";
+            return RedirectToAction("Update");
+        }
+
+        hospital.Name = name;
+        hospital.Telephone1 = telephone1;
+        hospital.Telephone2 = telephone2;
+        hospital.AddressId = addressId;
+
+        _context.Hospitals.Update(hospital);
+        _context.SaveChanges();
+
+        TempData["MessageSuccess"] = $"Hospital com ID {id} atualizado com sucesso.";
+        return RedirectToAction("Show", new { id = id });
     }
 }
